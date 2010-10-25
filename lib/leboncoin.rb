cefea67e70d44b2e914ce9fa405cb0c168e450a0
Karim DRIDI <seth.holth@gmail.com>
2010-10-25 17:27:52 +0200

    Upgrading v0.0.3: The project is now quite properly modulized

--- lib/leboncoin.rb
+++ lib/leboncoin.rb
@@ -1,135 +1,216 @@
-class Leboncoin
-  VERSION = '0.0.2'
+module Leboncoin
+  VERSION = '0.0.3'
 
-  def convert(str)
-    require 'htmlentities'
-    str = HTMLEntities.new.encode(str.force_encoding("ISO-8859-15").encode("UTF-8"), :hexadecimal)
-      .gsub(/&#xc3;&#xa9;/, "&eacute;")
-      .gsub(/&#xc2;&#x99;/, "&#153;")
-      .gsub(/&#xc2;&#xae;/, "&copy;")
-
-    str = HTMLEntities.new.decode(str)
-      .gsub(/\u0092/, "'")
-      .gsub(/\u0096/, "-")
-      .gsub(/\u0095/, "&#149;")
-      .gsub(/\u0099/, "&#153;")
-      .gsub(/\u0080/, "&euro;")
-
-    return str
-  end
-
-  def retrieveData(url)
-    require 'open-uri'
-    return open(url)
-  end
-
-  def createDocument(url)
-    require 'nokogiri'
-    return Nokogiri::HTML(retrieveData(url))
-  end
-
-  def createItem(node)
-    item = Hash.new
-
-    # DATE
-    require 'date'
-    item["dateFR"] = node.xpath('td[1]')[0].inner_html.strip
-    item["dateEN"] = item["dateFR"]
-      .gsub(/ ao.t<br>/, " aug<br>").gsub(/<br>/, " ")
-      .gsub(/Aujourd'hui/, (Date.today - 0).strftime('%d %b').downcase)
-      .gsub(/Hier/, (Date.today - 1).strftime('%d %b').downcase)
-
-    item["date"] = DateTime.parse(item["dateEN"])
-
-    # IMAGE
-    item["image"] = begin node.xpath('td[2]/table/tbody/tr[2]/td[2]/a/img')[0]["src"].strip rescue nil end
-
-    # NAME & LINK
-    item["title"] = convert(node.xpath('td[3]/a')[0].content.strip)
-    item["link"] = node.xpath('td[3]/a')[0]["href"].strip
-
-    # PRICE
-    item["price"] =  begin node.xpath('td[3]/text()[3]')[0].content.strip.gsub(/..$/, "").to_i rescue nil end
-    item["currency"] = "EUR"
-
-    # DESCRIPTION
-    doc = createDocument(item["link"])
-    item["description"] = convert(doc.xpath('//span[@class="lbcAd_text"]').inner_html)
-    item["city"] = doc.xpath('//span[@class="ad_details_400"]/strong').inner_html.strip
-    item["postcode"] = item["city"][/[0-9]+/]
-    item["city"] = item["city"].gsub(/[0-9]+ /, "")
-
-    return item
-  end
-
-  def parseItems(items, url, size)
-    doc = createDocument url
-  
-    continue = true
-    doc.xpath('//table[@id="hl"]/tr').each do |node|
-      if items.size < size
-        items.push createItem(node)
-      end
-    end
-
-    if items.size < size
-      doc.xpath('//a[starts-with(text(), "Page suivante")]').each do |node|
-        parseItems(items, node['href'], size)
-      end
+  class << self
+    def Search keywords, size = 10
+      Leboncoin::Search.parseItems "http://www.leboncoin.fr/occasions/?f=a&th=1&q=" + keywords, size
     end
   end
-
-  def createItems(url, size)
-    items = Array.new
-    parseItems(items, url, size)
-
-    return items
-  end
-
-  def createJSON(url, size)
-    require 'json'
-    return JSON.pretty_generate(createItems(url, size))
-  end
-
-  def createRSS(url, size)
-    require 'rss/maker'
-
-    content = RSS::Maker.make("2.0") do |m|
-      m.channel.title = "leboncoin.fr"
-      m.channel.link = url
-      m.channel.description = "leboncoin.fr"
-      m.items.do_sort = true
-      createItems(url, size).each do |item|
-        title = ""
-        price = ""
-        if item["price"] != nil
-          price = item["price"].to_s + " " + item["currency"]
-          title = "[" + price + "] "
-        end
-
-        postcode = ""
-        if item["postcode"] != nil
-          postcode = item["postcode"]
-        end
-
-        i = m.items.new_item
-        i.title = title + item["title"]
-        i.link = item["link"]
-        begin
-          i.description = convert("<img src='" + item["image"] + "'/><br/>" \
-            + "<b>Ville</b> : " + item["city"] + "<br/>" \
-            + "<b>Code postal</b> : " + postcode + "<br/>" \
-            + "<p>" + item["description"] + "</p><hr/>" \
-            + "<b>Prix</b> : " + price + "<hr/>")
-        rescue    
-          require 'json'
-          puts ">>>> " + JSON.pretty_generate(item)
-        end
-        i.date = Time.now #item["date"]
-      end
-    end
-  end
-
 end
 
-# puts Leboncoin.new.createJSON('http://www.leboncoin.fr/occasions/?f=a&th=1&q=dreamcast', 2)
+module Leboncoin
+  module Search
+    class << self
+      ###
+      # Load the given URL as a well-formed HTML document
+      def loadHTML url
+        require 'open-uri'
+        require 'nokogiri'
+
+        doc = begin
+          Nokogiri::HTML(open(url))
+        rescue
+          nil
+        end
+
+        return doc
+      end
+
+      ###
+      # Parse items from a given URL
+      def parseItems url, size, items = Leboncoin::Search::SearchItems.new(url)
+        doc = loadHTML url
+
+        doc.xpath('//table[@id="hl"]/tr').each do |node|
+          if items.size < size
+            parseItemNode node, items.createItem
+          end
+        end
+
+        if items.size < size
+          doc.xpath('//a[starts-with(text(), "Page suivante")]').each do |node|
+            parse node['href'], size, items
+          end
+        end
+
+        return items
+      end
+
+      ###
+      # Parse item from a given XML node.
+      def parseItemNode(node, item = Hash.new)
+        # DATE
+        item["date"] = begin
+          require 'date'
+          DateTime.parse(
+            node.xpath('td[1]')[0].inner_html.strip
+              .gsub(/ ao.t<br>/, " aug<br>").gsub(/<br>/, " ")
+              .gsub(/Aujourd'hui/, (Date.today - 0).strftime('%d %b').downcase)
+              .gsub(/Hier/, (Date.today - 1).strftime('%d %b').downcase)
+          )
+        rescue
+          nil
+        end
+
+        # IMAGE
+        item["image"] = begin
+          node.xpath('td[2]/table/tbody/tr[2]/td[2]/a/img')[0]["src"].strip
+        rescue
+          nil
+        end
+
+        # TITLE
+        item["title"] = begin
+          convert(node.xpath('td[3]/a')[0].content.strip)
+        rescue
+          "UNKNOW TITLE"
+        end
+
+        # LINK
+        item["link"] = begin
+          node.xpath('td[3]/a')[0]["href"].strip
+        rescue
+          nil
+        end
+
+        # PRICE
+        item["currency"] = "EUR"
+        item["price"] = begin
+          node.xpath('td[3]/text()[3]')[0].content.strip.gsub(/..$/, "").to_i
+        rescue
+          nil
+        end
+
+        return parseItem item["link"], item
+      end
+
+      ###
+      # Parse item from a given HTML link.
+      def parseItem url, item = Hash.new
+        doc = loadHTML url
+
+        # DESCRIPTION
+        item["description"] = begin
+          convert(doc.xpath('//span[@class="lbcAd_text"]').inner_html)
+        rescue
+          nil
+        end
+
+        value = begin
+          doc.xpath('//span[@class="ad_details_400"]/strong').inner_html.strip
+        rescue
+          nil
+        end
+
+        # POSTCODE
+        item["postcode"] = begin
+          value[/[0-9]+/]
+        rescue
+          nil
+        end
+
+        # CITY
+        item["city"] = begin
+          value.gsub(/[0-9]+ /, "")
+        rescue
+          nil
+        end
+
+        return item
+      end
+    end
+  end
+end
+
+module Leboncoin
+  module Search
+    module SearchItems
+      class << self
+        ###
+        # Default constructor
+        def new link
+          @link = link
+          @items = Array.new
+          return self
+        end
+
+        ###
+        # Create a new item
+        def createItem item = Hash.new
+          @items.push item
+          return item
+        end
+
+        def each
+          @items.each do |item|
+            yield item if block_given?
+          end
+        end
+
+        def size
+          return @items.size
+        end
+
+        def to_json
+          require 'json'
+          return JSON.pretty_generate(@items)
+        end
+
+        def to_rss
+          require 'rss/maker'
+
+          content = RSS::Maker.make("2.0") do |m|
+            m.channel.title = "leboncoin.fr"
+            m.channel.link = @link
+            m.channel.description = "leboncoin.fr"
+            m.items.do_sort = true
+            @items.each do |item|
+              price = ""
+              if item["price"] != nil
+                price = item["price"].to_s + " " + item["currency"]
+              end
+
+              postcode = ""
+              if item["postcode"] != nil
+                postcode = item["postcode"]
+              end
+
+              i = m.items.new_item
+              i.title = item["title"]
+              i.link = item["link"]
+              begin
+                i.description = convert("<img src='" + item["image"] + "'/><br/>" \
+                  + "<b>Ville</b> : " + item["city"] + "<br/>" \
+                  + "<b>Code postal</b> : " + postcode + "<br/>" \
+                  + "<p>" + item["description"] + "</p><hr/>" \
+                  + "<b>Prix</b> : " + price + "<hr/>")
+              rescue    
+                require 'json'
+                puts ">>>> ERROR >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>\n"
+                puts JSON.pretty_generate(item)
+                puts "\n<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<"
+              end
+              i.date = Time.now #item["date"]
+            end
+          end
+        end
+      end
+    end
+  end
+end
+
+#Leboncoin::Search('dreamcast', 2).each do |i|
+#  puts ">>> #{i['postcode']} : " + JSON.pretty_generate(i) + "\n\n"
+#end
+#puts Leboncoin::Search('dreamcast', 1).to_json
+#puts Leboncoin::Search('dreamcast', 1).to_rss
